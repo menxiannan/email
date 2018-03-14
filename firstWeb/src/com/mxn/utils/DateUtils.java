18d6e09d32a59d63efdee8fd6514391072054be2
menxiannan <120841789@qq.com>
2018-03-14 15:17:43 +0800

    简单的时间工具

---
+++ firstWeb/src/com/mxn/utils/DateUtils.java
@@ -0,0 +1,22 @@
+package com.mxn.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author Geenk-MXN
+ * @create 2018-03-14 15:13
+ */
+
+public class DateUtils {
+
+    private static final String ONE = "yyyy-MM-dd HH:mm:ss";
+
+    public static String getFormatDate(Date formatDate) {
+        if (null != formatDate) {
+            SimpleDateFormat sdf = new SimpleDateFormat(ONE);
+            return sdf.format(formatDate);
+        }
+        return null;
+    }
+}
