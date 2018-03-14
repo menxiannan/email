8874eeace07516c875a7a8c759284a3e190dd188
menxiannan <120841789@qq.com>
2018-03-14 15:50:22 +0800

    我就占着位置不动

---
+++ firstWeb/src/com/mxn/utils/SendEamilUtils.java
@@ -0,0 +1,12 @@
+package com.mxn.utils;
+
+/**
+ * @author Geenk-MXN
+ * @create 2018-03-14 15:47
+ */
+
+public class SendEamilUtils {
+    static {
+        System.out.println("我就占这位置不动！！！");
+    }
+}
